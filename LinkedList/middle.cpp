be9232ca4ca6c3ee1e6ccb0cf7532de1c5ca48dc
Krishna Agarwal <63241474+krishnaagarwal2001@users.noreply.github.com>
2026-02-14 15:22:08 +0000

    added comment

--- LinkedList/middle.cpp
+++ LinkedList/middle.cpp
@@ -8,6 +8,12 @@ Node* getMiddle(Node* head){
     Node* slow = head;
     Node* fast = head -> next;
 
+    /*
+        If we need 2nd element of middle in even length, change 
+        Node* fast = head -> next; to
+        Node* fast = head;
+    */
+
     while(fast != NULL){
         fast = fast -> next;
         if(fast != NULL){
