7146caeab4b81189bc9a71e93097f3a29f015ad3
Krishna Agarwal <63241474+krishnaagarwal2001@users.noreply.github.com>
2026-05-17 09:50:00 +0000

    Fixed graph cycle code

--- Graph_Codes/Cycle.cpp
+++ Graph_Codes/Cycle.cpp
@@ -1,117 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+vector<int>parent;
+vector<int>color;
 
-vector<ll>vis(n+1,0);
-        vll p(n+1,-1);
+bool isCycle = 0;
+vector<int> anyCycle;
 
-        function<void(int)> dfs=[&](ll st){
-            vis[st]=1;
-            // s.push(st);
-            for(auto it:g[st])
-            {
-                if(!vis[it])
-                {   
-                    p[it]=st;
-                    dfs(it);
-                }
-                else if(vis[it]==1)
-                {  	
-					//back edge
-                	if(it==st)
-                    {
-                        //self loop
-                        continue;
-                    }
-                    vll cycle;
-                    ll cur=st;
-                    cycle.pb(cur);
-                    while(p[cur]!=it)
-                    {
-                        cycle.pb(p[cur]);
-                        cur=p[cur];
-                    }
-                    cycle.pb(it);
-                    cycle.pb(st);
-                    reverse(all(cycle));
-                    cout<<sz(cycle)<<endl;
-                    for(auto y:cycle)
-                        cout<<y<<" ";
-     
-                    exit(0);
-                }
-				else if(vis[it]==2){
-					//cross edge
-				}
-            }
-            vis[st]=2;
+void dfs(int node, int par){
+    parent[node] = par;
+    color[node] = 2;
+
+    for(int v : g[node]){
+        if(v == par){           // we can comment it out in directed graph
+            continue;
+        }
+
+        if(color[v] == 1){
+            // node -- v is a forward edge
+            dfs(v,node);
+
+        }else if(color[v] == 2){
+            //node -- v is a back edge
+            // we found a cycle
             
-        };
+            if(!isCycle){
+                int curNode = node;
 
+                while(curNode != v){
+                    anyCycle.push_back(curNode);
+                    curNode = parent[curNode];
+                }
 
-bool detect_cycle(ll n)
-{
-	vis.assign(n + 1, 0);
-
-	bool cycle_found = 0;
-
-	for (int i = 1; i <= n; ++i)
-		if (!vis[i])
-			cycle_found |= dfs(i, -1);
-
-	return cycle_found;
+                anyCycle.push_back(curNode);
+            }
+        }else if(color[v] == 3){            // we can comment it out in undirected graph
+            //node -- v is a crosss edge
+        }
+    }
 }
 
-vi par;
-ll cyc_end, cyc_st;
-
-bool dfs1(ll i)
-{
-	vis[i] = 1;
-	for (auto nb : adj[i])
-	{
-		if (nb == par[i])
-			continue;
-
-		if (!vis[nb])
-		{
-			par[nb] = i;
-			bool cf = dfs1(nb);
-
-			if (cf)
-				return true;
-		}
-
-		else
-		{
-			cyc_end = i;
-			cyc_st = nb;
-			return true;
-		}
-	}
-
-	return false;
-}
-
-vector<ll> get_cycle(ll n)
-{
-	par.assign(n + 1, -1);
-	vis.assign(n + 1, false);
-
-	for (int i = 1; i <= n; ++i){
-	if (vis[i] == 0)
-	{
-		if (dfs1(i)){
-		//return the cycle
-		vector<ll> v;
-
-		while (cyc_end != cyc_st)
-		v.pb(cyc_end), cyc_end = par[cyc_end];
-
-		v.pb(cyc_st);
-
-		return v;
-		}
-	}
-	}
-
-	return {};
+int main(){
+    color.assign(n+1,1);
+    parent.resize(n+1);
 }
